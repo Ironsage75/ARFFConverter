6c864a462db58113c55764aa69da430db879ed0d
Amy Peerlinck <amy_linck@outlook.com>
2017-09-13 14:26:06 -0600

    added wine ARFF conversion result

--- src/converter/HeaderReader.java
+++ src/converter/HeaderReader.java
@@ -26,38 +26,40 @@ public class HeaderReader {
     public ArrayList<HeaderAttribute> readAttributeList(ArrayList<String> lines) {
         ArrayList<HeaderAttribute> headers = new ArrayList<>();
         for (String line : lines) {
-            //splits the string up at the space (indicated as how the user should input the information), into name and type
-            String[] split = line.split("--");
-            /**
-             * Check if the attribute is of type "date" in which case the user has the option to input a specific dateformat
-             * This gets saved as a DateAttribute, which extends from HeaderAttribute
-             */
-            if (split[1].equalsIgnoreCase("date")) {
-                DateAttribute attribute = null;
-                System.out.println("Enter dateformat for " + split[0] + ", if required, otherwise press enter:");
-                Scanner scanner = new Scanner(System.in);
-                if (!scanner.nextLine().isEmpty()) {
-                    String format = scanner.nextLine();
-                    attribute = new DateAttribute(split[0], split[1], format);
-                } else {
-                    attribute = new DateAttribute(split[0], split[1]);
+            if (!line.isEmpty()) {
+                //splits the string up at the space (indicated as how the user should input the information), into name and type
+                String[] split = line.split("--");
+                /**
+                 * Check if the attribute is of type "date" in which case the user has the option to input a specific dateformat
+                 * This gets saved as a DateAttribute, which extends from HeaderAttribute
+                 */
+                if (split[1].equalsIgnoreCase("date")) {
+                    DateAttribute attribute = null;
+                    System.out.println("Enter dateformat for " + split[0] + ", if required, otherwise press enter:");
+                    Scanner scanner = new Scanner(System.in);
+                    if (!scanner.nextLine().isEmpty()) {
+                        String format = scanner.nextLine();
+                        attribute = new DateAttribute(split[0], split[1], format);
+                    } else {
+                        attribute = new DateAttribute(split[0], split[1]);
+                    }
+                    headers.add(attribute);
                 }
-                headers.add(attribute);
-            }
-            /**
-             * Check if the attribute is of type "class", in which case the user has to input the different classes
-             * This gets saved as a ClassAttribute, which extends from HeaderAttribute
-             */
-            else if (split[1].equalsIgnoreCase("class")) {
-                ClassAttribute attribute = null;
-                System.out.println("Enter classnames for " + split[0] + ", seperated by commas:");
-                Scanner scanner2 = new Scanner(System.in);
-                String classes = scanner2.nextLine();
-                String[] splitClasses = classes.split(",");
-                attribute = new ClassAttribute(split[0], split[1], splitClasses);
-                headers.add(attribute);
-            } else {
-                headers.add(new HeaderAttribute(split[0], split[1]));
+                /**
+                 * Check if the attribute is of type "class", in which case the user has to input the different classes
+                 * This gets saved as a ClassAttribute, which extends from HeaderAttribute
+                 */
+                else if (split[1].equalsIgnoreCase("class")) {
+                    ClassAttribute attribute = null;
+                    System.out.println("Enter classnames for " + split[0] + ", seperated by commas:");
+                    Scanner scanner2 = new Scanner(System.in);
+                    String classes = scanner2.nextLine();
+                    String[] splitClasses = classes.split(",");
+                    attribute = new ClassAttribute(split[0], split[1], splitClasses);
+                    headers.add(attribute);
+                } else {
+                    headers.add(new HeaderAttribute(split[0], split[1]));
+                }
             }
         }
         return headers;
